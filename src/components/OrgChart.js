32620afe83c6904971e859b3f8aa9c8faea702a5
Bradley Adams <bradley.wayne.adams@gmail.com>
2022-03-23 06:14:04 +1300

    update org chart

--- src/components/OrgChart.js
+++ src/components/OrgChart.js
@@ -1,46 +1,26 @@
-import React, { Component } from 'react'
-import Chart from 'react-google-charts'
-const OrgData = [
-  ['Name', 'Manager', 'ToolTip'],
-  [
-    {
-      v: 'Stuart Wilson',
-      f: 'Lisa<div style="color:red; font-style:italic">CEO</div>',
-    },
-    '',
-    'The President',
-  ],
-  [
-    {
-      v: 'Eva',
-      f: 'Eva<div style="color:red; font-style:italic">Vice President</div>',
-    },
-    'Lisa',
-    'VP',
-  ],
-  ['Alice', 'Lisa', ''],
-  ['Bob', 'Eva', 'Bob Sponge'],
-  ['Carol', 'Bob', ''],
-]
+import React, { Component } from "react";
+import Chart from "react-google-charts";
+import { OrgData } from "./StaticOrgData"
+
 const OrgOptions = {
   allowHtml: true,
-}
+};
 class OrgChart extends Component {
   render() {
     return (
       <div className="container mt-5">
         <h2>React Organization Chart Example</h2>
         <Chart
-          width={'100%'}
+          width={"100%"}
           height={400}
           chartType="OrgChart"
           loader={<div>Loading Chart</div>}
           data={OrgData}
           options={OrgOptions}
-          rootProps={{ 'data-testid': '1' }}
+          rootProps={{ "data-testid": "1" }}
         />
       </div>
-    )
+    );
   }
 }
-export default OrgChart
+export default OrgChart;
