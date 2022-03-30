212c475ba3fef7f03768beacc81b920b4c22e8f1
Bradley Adams <bradley.wayne.adams@gmail.com>
2022-03-30 21:59:52 +1300

    Create a GraghQL API and database

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "tlombapi": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
