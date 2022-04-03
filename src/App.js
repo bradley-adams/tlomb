1cfdf525edbb7a934ce2668f653695bce9e9cef8
Bradley Adams <bradley.wayne.adams@gmail.com>
2022-04-03 12:38:31 +1200

    splitting out todo from auth

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import Amplify, { API, graphqlOperation } from 'aws-amplify'
 import { createTodo } from './graphql/mutations'
 import { listTodos } from './graphql/queries'
+import Todo from './Todo/Todo'
 
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
@@ -32,18 +33,6 @@ const App = () => {
     } catch (err) { console.log('error fetching todos') }
   }
 
-  async function addTodo() {
-    try {
-      if (!formState.name || !formState.description) return
-      const todo = { ...formState }
-      setTodos([...todos, todo])
-      setFormState(initialState)
-      await API.graphql(graphqlOperation(createTodo, {input: todo}))
-    } catch (err) {
-      console.log('error creating todo:', err)
-    }
-  }
-
   return (
     <Authenticator>
       {({ signOut, user }) => (
@@ -51,28 +40,7 @@ const App = () => {
           <h1>Hello {user.username}</h1>
           <button style={styles.button} onClick={signOut}>Sign out</button>
           <br />
-          <h2>Amplify Todos</h2>
-          <input
-            onChange={event => setInput('name', event.target.value)}
-            style={styles.input}
-            value={formState.name}
-            placeholder="Name"
-          />
-          <input
-            onChange={event => setInput('description', event.target.value)}
-            style={styles.input}
-            value={formState.description}
-            placeholder="Description"
-          />
-          <button style={styles.button} onClick={addTodo}>Create Todo</button>
-          {
-            todos.map((todo, index) => (
-              <div key={todo.id ? todo.id : index} style={styles.todo}>
-                <p style={styles.todoName}>{todo.name}</p>
-                <p style={styles.todoDescription}>{todo.description}</p>
-              </div>
-            ))
-          }
+          <Todo/>
         </div>
       )}
     </Authenticator>
