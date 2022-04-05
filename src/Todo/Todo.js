0c2db29a98616ad2f8ba63022b36a457464feb9b
Bradley Adams <bradley.wayne.adams@gmail.com>
2022-04-05 21:09:00 +1200

    moving styles from todo.js to todo.scss

--- src/Todo/Todo.js
+++ src/Todo/Todo.js
@@ -50,55 +50,27 @@ const Todo = () => {
       <h2>Amplify Todos</h2>
       <input
         onChange={(event) => setInput("name", event.target.value)}
-        style={styles.input}
+        id="input"
         value={formState.name}
         placeholder="Name"
       />
       <input
         onChange={(event) => setInput("description", event.target.value)}
-        style={styles.input}
+        id="input"
         value={formState.description}
         placeholder="Description"
       />
-      <button style={styles.button} onClick={addTodo}>
+      <button id="button" onClick={addTodo}>
         Create Todo
       </button>
       {todos.map((todo, index) => (
-        <div key={todo.id ? todo.id : index} style={styles.todo}>
-          <p style={styles.todoName}>{todo.name}</p>
-          <p style={styles.todoDescription}>{todo.description}</p>
+        <div id="todo" key={todo.id ? todo.id : index}>
+          <p id="todoName">{todo.name}</p>
+          <p id="todoDescription">{todo.description}</p>
         </div>
       ))}
     </div>
   );
-};
-
-const styles = {
-  container: {
-    width: 400,
-    margin: "0 auto",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    padding: 20,
-  },
-  todo: { marginBottom: 15 },
-  input: {
-    border: "none",
-    backgroundColor: "#ddd",
-    marginBottom: 10,
-    padding: 8,
-    fontSize: 18,
-  },
-  todoName: { fontSize: 20, fontWeight: "bold" },
-  todoDescription: { marginBottom: 0 },
-  button: {
-    backgroundColor: "black",
-    color: "white",
-    outline: "none",
-    fontSize: 18,
-    padding: "12px 0px",
-  },
 };
 
 export default Todo;
