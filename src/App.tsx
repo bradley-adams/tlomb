1f5270e9d3e945ba11ba7909bd9586afb14becb9
Bradley Adams <bradley.wayne.adams@gmail.com>
2022-04-19 20:51:37 +1200

    got the ts shoing in localhost

--- src/App.tsx
+++ src/App.tsx
@@ -5,102 +5,77 @@ import "./App.css";
 import Amplify from "aws-amplify";
 import { listTodos } from "./graphql/queries";
 import { ListTodosQuery, OnCreateTodoSubscription } from "./API";
+import Todo, {
+  mapListTodosQuery,
+  mapOnCreateTodoSubscription,
+} from "./models/todo";
 
-import { createTodo } from "./graphql/mutations";
+import config from "./aws-exports";
+import callGraphQL, { subscribeGraphQL } from "./models/graphql-api";
+import CreateTodo from "./components/create-todo";
 
-import awsExports from "./aws-exports";
-Amplify.configure(awsExports);
+import { onCreateTodo } from "./graphql/subscriptions";
 
-const initialState = { name: "", description: "" };
+Amplify.configure(config);
 
-const App = () => {
-  const [formState, setFormState] = useState(initialState);
-  const [todos, setTodos] = useState([]);
+function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   useEffect(() => {
-    fetchTodos();
+    async function getData() {
+      try {
+        const todoData = await callGraphQL<ListTodosQuery>(listTodos);
+        const todos = mapListTodosQuery(todoData);
+        setTodos(todos);
+      } catch (error) {
+        console.error("Error fetching todos", error);
+      }
+    }
+    getData();
   }, []);
 
-  function setInput(key: string, value: string) {
-    setFormState({ ...formState, [key]: value });
-  }
+  // useEffect(() => {
+  //   // @ts-ignore
+  //   const subscription = API.graphql(graphqlOperation(onCreateTodo)).subscribe({
+  //     next: (response: SubscriptionValue<OnCreateTodoSubscription>) => {
+  //       const todo = mapOnCreateTodoSubscription(response.value.data);
+  //       console.log(todo);
+  //       setTodos([...todos, todo]);
+  //     },
+  //   });
 
-  async function fetchTodos() {
-    try {
-      const todoData = await API.graphql(graphqlOperation(listTodos));
-      const todos = todoData.data.listTodos.items;
-      setTodos(todos);
-    } catch (err) {
-      console.log("error fetching todos");
-    }
-  }
+  //   return () => subscription.unsubscribe();
+  // });
 
-  async function addTodo() {
-    try {
-      if (!formState.name || !formState.description) return;
-      const todo = { ...formState };
-      setTodos([...todos, todo]);
-      setFormState(initialState);
-      await API.graphql(graphqlOperation(createTodo, { input: todo }));
-    } catch (err) {
-      console.log("error creating todo:", err);
-    }
-  }
+  const onCreateTodoHandler = (
+    createTodoSubscription: OnCreateTodoSubscription
+  ) => {
+    const todo = mapOnCreateTodoSubscription(createTodoSubscription);
+    setTodos([...todos, todo]);
+  };
+
+  useEffect(() => {
+    const subscription = subscribeGraphQL<OnCreateTodoSubscription>(
+      onCreateTodo,
+      onCreateTodoHandler
+    );
+
+    return () => subscription.unsubscribe();
+  }, [todos]);
 
   return (
-    <div style={styles.container}>
-      <h2>Amplify Todos</h2>
-      <input
-        onChange={(event) => setInput("name", event.target.value)}
-        style={styles.input}
-        value={formState.name}
-        placeholder="Name"
-      />
-      <input
-        onChange={(event) => setInput("description", event.target.value)}
-        style={styles.input}
-        value={formState.description}
-        placeholder="Description"
-      />
-      <button style={styles.button} onClick={addTodo}>
-        Create Todo
-      </button>
-      {todos.map((todo, index) => (
-        <div key={todo.id ? todo.id : index} style={styles.todo}>
-          <p style={styles.todoName}>{todo.name}</p>
-          <p style={styles.todoDescription}>{todo.description}</p>
+    <div className="App">
+      <CreateTodo />
+
+      <h2>Todos:</h2>
+      {todos?.map((t) => (
+        <div key={t.id}>
+          <h2>{t.name}</h2>
+          <p>{t.description}</p>
         </div>
       ))}
     </div>
   );
-};
-
-const styles = {
-  container: {
-    width: 400,
-    margin: "0 auto",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    padding: 20,
-  },
-  todo: { marginBottom: 15 },
-  input: {
-    border: "none",
-    backgroundColor: "#ddd",
-    marginBottom: 10,
-    padding: 8,
-    fontSize: 18,
-  },
-  todoName: { fontSize: 20, fontWeight: "bold" },
-  todoDescription: { marginBottom: 0 },
-  button: {
-    backgroundColor: "black",
-    color: "white",
-    outline: "none",
-    fontSize: 18,
-    padding: "12px 0px",
-  },
-};
+}
 
 export default App;
