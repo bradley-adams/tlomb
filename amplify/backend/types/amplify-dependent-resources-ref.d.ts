9ef962a026c7e2548121f04a0b03f780de2dd6c0
Bradley Adams <bradley.wayne.adams@gmail.com>
2022-04-25 15:33:02 +1200

    removing existing code

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "tlomb": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
